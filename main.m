a8bdf2c3db1271a38fe9940e26d0aa449f21f68a
LocatorOfTangos <70209292+LocatorOfTangos@users.noreply.github.com>
2022-07-19 20:43:53 +1000

    Added Joshua's land discretisation visualisation code

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 %% Code 
 c = load('coastlines'); 
-[lon,lat] = meshgrid(linspace(-179, 179, 180), linspace(-89.5,89.5, 180)); 
+[lon,lat] = meshgrid(linspace(-179, 179, 18), linspace(-89.5,89.5, 18)); 
 %[lon,lat] = meshgrid(linspace(-175, 175, 18), linspace(-85,85, 18)); 
-land = landmask(lat,lon)*1 
-worldmap('world') 
-h=pcolorm(lat,lon,land)
+land = landmask(lat,lon)*1;
+worldmap('world') ;
+h=pcolorm(lat,lon,land);
